ba49ff791eae91e3d8c239c2cc7ec8f1e1c90ce5
ye.voitsekhovska <sunakojane6@gmail.com>
2025-08-08 18:07:35 +0300

    Added keyboard navigation

--- script.js
+++ script.js
@@ -2,20 +2,51 @@
 
 const questionBoxes = document.querySelectorAll(".question-box");
 
-questionBoxes.forEach((box) => {
+questionBoxes.forEach((box, index) => {
   const answer = box.querySelector("p");
   const question = box.querySelector(".question-title");
   const icon = box.querySelector("img");
 
-  question.addEventListener("click", () => {
+  question.setAttribute("tabindex", "0");
+
+  const toggleAnswers = () => {
     const isVisible = !answer.classList.contains("hidden");
 
-    document.querySelectorAll(".question-box p").forEach((p) => p.classList.add("hidden"));
-    document.querySelectorAll(".question-title img").forEach((img) => (img.src = "./assets/images/icon-plus.svg"));
+    document
+      .querySelectorAll(".question-box p")
+      .forEach((p) => p.classList.add("hidden"));
+    document
+      .querySelectorAll(".question-title img")
+      .forEach((img) => (img.src = "./assets/images/icon-plus.svg"));
 
     if (!isVisible) {
       answer.classList.remove("hidden");
       icon.src = "./assets/images/icon-minus.svg";
+    }
+  };
+
+  question.addEventListener("click", toggleAnswers);
+
+  question.addEventListener("keydown", (e) => {
+    if (e.key === "Enter" || e.key === " ") {
+      e.preventDefault();
+
+      toggleAnswers();
+    }
+
+    if (e.key === "ArrowDown") {
+      e.preventDefault();
+
+      const nextIndex = (index + 1) % questionBoxes.length;
+      questionBoxes[nextIndex].querySelector(".question-title").focus();
+    }
+
+    if (e.key === "ArrowUp") {
+      e.preventDefault();
+
+      const prevIndex =
+        (index - 1 + questionBoxes.length) % questionBoxes.length;
+      questionBoxes[prevIndex].querySelector(".question-title").focus();
     }
   });
 });
